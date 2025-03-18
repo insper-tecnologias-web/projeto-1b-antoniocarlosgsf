e025faceda218b63aa97082d9b780890d88aa766
antoniocarlosgsf <antoniocarlos171201@gmail.com>
2025-03-18 09:26:27 -0300

    delete e update implementados

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -11,3 +11,24 @@ def index(request):
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request, id):
+    note = Note.objects.get(id=id)
+    note.delete()
+    return redirect('index')
+
+def update(request, id):
+    nota = Note.objects.get(id=id)
+
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+
+        nota.title = title
+        nota.content = content
+
+        nota.save()
+        return redirect('index')
+
+    elif request.method == 'GET':
+        return render(request, 'notes/update.html', {'note': nota})
